3e5611bd7bc27111f5b0934b2e833edff51ba89b
Gulshan Kumar Thakur <96992693+ga31na@users.noreply.github.com>
2024-11-16 22:01:53 +0530

    Update App.js

--- Micro Project 2/App.js
+++ Micro Project 2/App.js
@@ -1,1 +1,13 @@
-1
+import React from "react";
+import Calculator from "./components/Calculator";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
